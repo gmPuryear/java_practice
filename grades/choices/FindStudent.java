82c56b2774b07330545dfd411acdb0192073b66b
George Puryear <gm.puryear@yahoo.com>
2023-09-05 23:17:10 -0500

    finished validating a student's name when user is searching via name

--- grades/choices/FindStudent.java
+++ grades/choices/FindStudent.java
@@ -7,29 +7,52 @@ import java.util.HashMap;
 import static grades.GradesApplication.scanner;
 
 public class FindStudent {
-    public static void validateUsername(String studentName) {
-        checkNameForNum(studentName);
-        char[] splitStudentCharArr = studentName.toCharArray();
-        String splitStudentName[] = (studentName.split(" ")); // student name split on whitespace and assigned as an array
-        if (splitStudentName.length != 2 ) {
-            System.out.println("Please enter a valid name (e.g.\"Jane Doe\"): ");
-        }
-
-    }
-
-    public static void checkNameForNum(String studentName) {
-        char[] splitStudentCharArr = studentName.toCharArray();
-        for (int i = 0; i < splitStudentCharArr.length; i++) {
-            isDigit("1");
-
-        }
-    }
 
     public static void findStudent(HashMap<String, Student> students) {
         System.out.println("Please enter students first and last name (e.g.\"Jane Doe\"): ");
         String studentName = scanner.nextLine();
-        validateUsername(studentName);
+//        checkNameIsTwoWords(studentName);
+//        checkNameForNums(studentName);
+        if (checkNameIsTwoWords(studentName) && checkNameForNums(studentName)) {
+            System.out.println("Name is good!");
+        } else {
+            System.out.println("name is bad!");
+        }
     }
+
+    public static boolean checkNameIsTwoWords(String studentName) {
+        String splitStudentName[] = (studentName.split(" ")); // student name split on whitespace and assigned as an array
+        if (splitStudentName.length != 2) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+//    public static boolean checkNameForNums(String studentName) {
+//        char[] splitStudentCharArr = studentName.toCharArray();
+//        boolean nameHasNoNums = true;
+//        while (nameHasNoNums)
+//            for (int i = 0; i < splitStudentCharArr.length; i++) {
+//                if (Character.isDigit(splitStudentCharArr[i])) {
+//                    nameHasNoNums = false;
+//                    break;
+//                }
+//            }
+//        return nameHasNoNums;
+//    }
+
+    public static boolean checkNameForNums(String studentName) {
+        char[] splitStudentCharArr = studentName.toCharArray();
+        for (int i = 0; i < splitStudentCharArr.length; i++) {
+            if (Character.isDigit(splitStudentCharArr[i])) {
+                    return false;
+                }
+        }
+        return true;
+    }
+
+
 
 
 
